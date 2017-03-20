b19336f1da0e809b79a63d6c27687d6e4253ece3
miaomiao8890 <miaomiao8890@vip.qq.com>
2017-03-20 16:26:32 +0800

    update

--- app/controllers/pwa/index.js
+++ app/controllers/pwa/index.js
@@ -23,19 +23,25 @@ exports.getBeautyDirectoryAll = function(req, res) {
 exports.getBeautyDirectoryDetail = function(req, res){
   var id = req.params.id;
 
-  BeautyDirectory.findById(id, function(err, beautyDirectory){
-    if(err){
-      res.json({
-        status_code: 500,
-        message: err,
-        result: {}
-      });
-    } else {
-      res.json({
-        status_code: 200,
-        message: 'ok',
-        result: beautyDirectory
-      });
-    }
+  BeautyDirectory
+    .findOne({_id: id})
+    .populate({
+      path: "beauty_photos",
+      select: "url"
+    })
+    .exec(function(err, beautyDirectory){
+      if(err){
+        res.json({
+          status_code: 500,
+          message: err,
+          result: {}
+        });
+      } else {
+        res.json({
+          status_code: 200,
+          message: 'ok',
+          result: beautyDirectory
+        });
+      }
   });
 }
